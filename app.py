0d28597f0a2c2609beffbbf0bef3dd80f3bf8b08
Prince11442B <90135prince@gmail.com>
2026-08-29 11:41:56 +0530

    Create app.py and import

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+from src import config
+from src.ingestor import extract_wildfires,extract_storms
+from src.engineer import merge_and_engineer,prepare_for_ml
+from src.modeler import train_and_predict
+from src.evaluator import evaluate_and_plot
