618529204bfd373de4fddec60158a5e80e4712aa
saran220407-max <saran.kumar220407@gmail.com>
2026-08-29 12:33:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import sys
 from src import config
 from src.ingestor import extract_wildfires, extract_storms
+from src.engineer import merge_and_engineer, prepare_for_ml
+from src.modeler import train_and_predict
+from src.evaluator import evaluate_and_plot
