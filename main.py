8bac7f1469e98c5c5294d38f6bdd3632cba80cc9
kushagrbhatia0315-dot <kushagrbhatia0315@gmail.com>
2026-08-30 11:14:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
         wf_df = extract_wildfires(config.WILDFIRE_DB, config.ROW_LIMIT, config.WF_FEATURES, config.WF_TARGET)
         storm_df = extract_storms(config.STORMS_DIR, config.STORM_FEATURES)
         merged_df = merge_and_engineer(wf_df, storm_df, config.WF_TARGET)
-        merged_df.to_csv("api_disaster_data.csv", index=False)
+        wf_df.to_csv("api_disaster_data.csv", index=False)
         X_train, X_test, y_train, y_test = prepare_for_ml(
             df=merged_df, target_col=config.WF_TARGET, test_size=config.TEST_SIZE
         )
