ccf940740e212b13db40854a64947b23065b9426
Prince11442B <90135prince@gmail.com>
2026-08-30 16:38:49 +0530

    Update ingestor.py

--- src/ingestor.py
+++ src/ingestor.py
@@ -2,7 +2,6 @@ import sqlite3
 import pandas as pd
 import os
 import glob
-
 def extract_wildfires(db_path: str, limit: int, features: list, target: str) -> pd.DataFrame:
     print("--> [1/5] Extracting Wildfire Database...")
     if not os.path.exists(db_path):
